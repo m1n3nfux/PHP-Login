afad57608443918d15143f511d47f698f03920a0
Caspar von Beöczy <caspar@librito.de>
2023-02-16 17:17:22 +0100

    differentiate between root and other users

--- usermanagement.php
+++ usermanagement.php
@@ -19,36 +19,53 @@ require 'config.php';
         echo $config["loginTitle"];
         ?>
     </h1>
-    <div id="users">
-        <h3>Users</h3>
-        <?php
+    <?php
+        if($_SESSION["user"] == "root") {
+            echo "<div id='users'>
+                <h3>Users</h3>";
             $login_data = include('login-data.php');
-            foreach ($login_data as $key => $value){
-                echo "<p class='user'>".$key."</p>";
+            foreach ($login_data as $key => $value) {
+                echo "<p class='user'>" . $key . "</p>";
             }
-        ?>
-    </div>
+            echo "</div>";
+        } else {
+            echo "<h3>Hello ".$_SESSION["user"]."</h3>";
+        }
+    ?>
     <form method="post">
         <h2>Change Password</h2>
-        <input type="text" name="update-user_name" placeholder="Username" required>
+        <?php
+            if($_SESSION["user"] == "root"){
+                echo "<input type='text' name='update-user_name' placeholder='Username' required>";
+            }
+        ?>
         <input type="password" name="update-user_cpw" placeholder="Current Password" required>
         <input type="password" name="update-user_pw" placeholder="New Password" required>
         <input type="password" name="update-user_pwr" placeholder="Repeat New Password" required>
         <input type="submit" name="update-user_submit" value="Submit">
     </form>
+    <?php
+    if($_SESSION["user"] == "root"){
+        echo "<form method='post'>
+            <h2>Add User</h2>
+            <input type='text' name='add-user_name' placeholder='Username' required>
+            <input type='password' name='add-user_pw' placeholder='New Password' required>
+            <input type='password' name='add-user_pwr' placeholder='Repeat New Password' required>
+            <input type='submit' name='add-user_submit' value='Submit'>
+        </form>";
+    }
+    ?>
+
 
     <form method="post">
-        <h2>Add User</h2>
-        <input type="text" name="add-user_name" placeholder="Username" required>
-        <input type="password" name="add-user_pw" placeholder="New Password" required>
-        <input type="password" name="add-user_pwr" placeholder="Repeat New Password" required>
-        <input type="submit" name="add-user_submit" value="Submit">
-    </form>
+        <h2>Delete Account</h2>
 
-    <form method="post">
-        <h2>Delete User</h2>
-        <input type="text" name="delete-user_name" placeholder="Username">
-        <input type="submit" name="delete-user_submit" value="Submit">
+        <?php
+        if($_SESSION["user"] == "root"){
+            echo "<input type='text' name='delete-user_name' placeholder='Username'>";
+        }
+        ?>
+        <input type="submit" name="delete-user_submit" value="Delete">
     </form>
 
     <form method="post">
@@ -59,12 +76,15 @@ require 'config.php';
 
 <?php
 if(isset($_POST["logout"])){
-    echo 'logout.php';
+    include 'logout.php';
 }
 
 
 if(isset($_POST["update-user_submit"])){
     $login_data = include('login-data.php');
+    if($_SESSION["user"] != "root"){
+        $_POST["update-user_name"] = $_SESSION["user"];
+    }
     if (password_verify($_POST["update-user_cpw"], $login_data[$_POST["update-user_name"]])) {
         if($_POST["update-user_pw"] == $_POST["update-user_pwr"]){
             replace_password_hash($_POST["update-user_name"], $_POST["update-user_pw"]);
@@ -89,19 +109,29 @@ if(isset($_POST["add-user_submit"])){
 
 if(isset($_POST["delete-user_submit"])){
     $file = 'login-data.php';
+    if($_SESSION["user"] != "root"){
+        $_POST["delete-user_name"] = $_SESSION["user"];
+    }
     $login_data = include($file);
-    if(sizeof($login_data)>1){
-        unset($login_data[$_POST["delete-user_name"]]);
-        $data = "<?php\nreturn [\n";
-        foreach($login_data as $key => $value) {
-            $escaped_key = str_replace(['$', '"'], ['\$', '\"'], $key);
-            $escaped_value = str_replace(['$', '"'], ['\$', '\"'], $value);
-            $data .= "\t\"{$escaped_key}\" => \"{$escaped_value}\",\n";
+    if($_POST["delete-user_name"] != "root") {
+        if (sizeof($login_data) > 1) {
+            unset($login_data[$_POST["delete-user_name"]]);
+            $data = "<?php\nreturn [\n";
+            foreach ($login_data as $key => $value) {
+                $escaped_key = str_replace(['$', '"'], ['\$', '\"'], $key);
+                $escaped_value = str_replace(['$', '"'], ['\$', '\"'], $value);
+                $data .= "\t\"{$escaped_key}\" => \"{$escaped_value}\",\n";
+            }
+            $data .= "];";
+            file_put_contents($file, $data);
+        } else {
+            echo "<h3 class='center'>Must have at least one User</h3>";
         }
-        $data .= "];";
-        file_put_contents($file, $data);
     } else {
-        echo "<h3 class='center'>Must have at least one User</h3>";
+        echo "<h3 class='center'>You can't delete the root user</h3>";
+    }
+    if($_SESSION["user"] != "root"){
+        include 'logout.php';
     }
 
 }
